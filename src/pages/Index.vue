b4c5bd947ac16962ee47aace7a12dfb0bfbffca0
Paul <paulozyzniewski@bigfootdigital.co.uk>
2019-11-08 16:28:46 +0000

    Add <main role='main'> wrapper for all the content

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,25 +1,27 @@
 <template>
   <Layout>
-    <section class="hero">
-      <div class="hero__contentWrapper">
-        <p class="hero__lead">Hello</p>
-        <h2 class="hero__title">It's Paul</h2>
-        <p class="hero__desc">The Front End Developer you need!</p>
-      </div>
+    <main role="main">
+      <section class="hero">
+        <div class="hero__contentWrapper">
+          <p class="hero__lead">Hello</p>
+          <h2 class="hero__title">It's Paul</h2>
+          <p class="hero__desc">The Front End Developer you need!</p>
+        </div>
 
-      <div class="hero__socialIcons socialIcons">
-        <a href="#" class="socialIcons_link">
-          <font-awesome class="socialIcons__icon" :icon="['fab', 'github']" />
-        </a>
-        <a href="#" class="socialIcons_link">
-          <font-awesome class="socialIcons__icon" :icon="['fab', 'linkedin']" />
-        </a>
-      </div>
+        <div class="hero__socialIcons socialIcons">
+          <a href="#" class="socialIcons_link">
+            <font-awesome class="socialIcons__icon" :icon="['fab', 'github']" />
+          </a>
+          <a href="#" class="socialIcons_link">
+            <font-awesome class="socialIcons__icon" :icon="['fab', 'linkedin']" />
+          </a>
+        </div>
 
-      <div class="hero__scrollBtn">
-        <font-awesome :icon="['fas', 'arrow-down']" />
-      </div>
-    </section>
+        <div class="hero__scrollBtn">
+          <font-awesome :icon="['fas', 'arrow-down']" />
+        </div>
+      </section>
+    </main>
   </Layout>
 </template>
 
