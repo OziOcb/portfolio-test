79c81df4c547e0feeb576e64c71f122d28ec7f09
Paul <paulozyzniewski@bigfootdigital.co.uk>
2019-11-12 10:56:06 +0000

    Instal and setup Husky, Lint-staged & Prettier plugins

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="layout">
-    <header class="header">
-      <div class="header__inner container">
-        <strong>
-          <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-        </strong>
-        <nav class="nav">
-          <g-link class="nav__link" to="/">Home</g-link>
-          <g-link class="nav__link" to="/about/">About</g-link>
-        </nav>
-      </div>
-    </header>
-    <slot />
-  </div>
+	<div class="layout">
+		<header class="header">
+			<div class="header__inner container">
+				<strong>
+					<g-link to="/">{{ $static.metadata.siteName }}</g-link>
+				</strong>
+				<nav class="nav">
+					<g-link class="nav__link" to="/">Home</g-link>
+					<g-link class="nav__link" to="/about/">About</g-link>
+				</nav>
+			</div>
+		</header>
+		<slot />
+	</div>
 </template>
 
 <static-query>
@@ -25,30 +25,29 @@ query {
 
 <style lang="scss">
 body {
-  font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto,
-    "Helvetica Neue", Arial, sans-serif;
-  margin: 0;
-  padding: 0;
-  line-height: 1.5;
+	margin: 0;
+	padding: 0;
+	font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+	line-height: 1.5;
 }
 
 .header {
-  position: fixed;
-  background-color: #fff;
-  top: 0;
-  left: 0;
-  right: 0;
+	position: fixed;
+	top: 0;
+	right: 0;
+	left: 0;
+	background-color: #fff;
 
-  &__inner {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 20px;
-    height: 80px;
-  }
+	&__inner {
+		margin-bottom: 20px;
+		display: flex;
+		height: 80px;
+		justify-content: space-between;
+		align-items: center;
+	}
 }
 
 .nav__link {
-  margin-left: 20px;
+	margin-left: 20px;
 }
 </style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,43 +1,44 @@
 <template>
-  <Layout>
-    <main role="main">
-      <section class="hero">
-        <div class="hero__contentWrapper">
-          <p class="hero__lead">Hello</p>
-          <h2 class="hero__title">It's Paul</h2>
-          <p class="hero__desc">The Front End Developer you need!</p>
-        </div>
+	<Layout>
+		<main role="main">
+			<section class="hero">
+				<div class="hero__contentWrapper">
+					<p class="hero__lead">Hello</p>
+					<h2 class="hero__title">It's Paul</h2>
+					<p class="hero__desc">The Front End Developer you need!</p>
+				</div>
 
-        <div class="hero__socialIcons socialIcons">
-          <a href="#" class="socialIcons_link">
-            <font-awesome class="socialIcons__icon" :icon="['fab', 'github']" />
-          </a>
-          <a href="#" class="socialIcons_link">
-            <font-awesome class="socialIcons__icon" :icon="['fab', 'linkedin']" />
-          </a>
-        </div>
+				<div class="hero__socialIcons socialIcons">
+					<a href="#" class="socialIcons_link">
+						<font-awesome class="socialIcons__icon" :icon="['fab', 'github']" />
+					</a>
+					<a href="#" class="socialIcons_link">
+						<font-awesome class="socialIcons__icon" :icon="['fab', 'linkedin']" />
+					</a>
+				</div>
 
-        <div class="hero__scrollBtn">
-          <font-awesome :icon="['fas', 'arrow-down']" />
-        </div>
-      </section>
-    </main>
-  </Layout>
+				<div class="hero__scrollBtn">
+					<font-awesome :icon="['fas', 'arrow-down']" />
+				</div>
+			</section>
+		</main>
+	</Layout>
 </template>
 
 <script>
 export default {
-  metaInfo: {
-    title: "Ozi Ocb Portfolio"
-  }
+	metaInfo: {
+		title: "Ozi Ocb Portfolio"
+	}
 };
 </script>
 
 <style lang="scss">
 .hero {
-  background-color: #900;
-  height: 100vh;
-  display: grid;
-  // TODO: Add the Grid
+	display: grid;
+	height: 100vh;
+	background-color: #900;
+
+	// TODO: Add the Grid
 }
 </style>
